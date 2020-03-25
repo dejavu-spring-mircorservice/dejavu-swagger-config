2da573291db96039ffa55bd15ce43c9f60600049
komal3770 <komal3770@gmail.com>
2020-03-26 02:13:11 +0530

    old code

--- src/main/java/com/dejavu/swagger/DejavuSwaggerConfigApplication.java
+++ src/main/java/com/dejavu/swagger/DejavuSwaggerConfigApplication.java
@@ -2,18 +2,36 @@ package com.dejavu.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableEurekaClient
-@EnableScheduling
-public class DejavuSwaggerConfigApplication {
-
+public class DejavuSwaggerConfigApplication extends SpringBootServletInitializer{
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.dejavu"))              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DejavuSwaggerConfigApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DejavuSwaggerConfigApplication.class, args);
 	}
